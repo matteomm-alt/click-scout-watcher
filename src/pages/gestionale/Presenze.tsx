1a3f91de6830db26b029a994e20d5092b331b0c1
matteomm-alt <matteo.m.m@gmail.com>
2026-04-18 19:29:28 +0200

    Update Presenze.tsx

--- src/pages/gestionale/Presenze.tsx
+++ src/pages/gestionale/Presenze.tsx
@@ -1,5 +1,3 @@
-import { ClipboardCheck } from 'lucide-react';
-import PlaceholderPage from '@/components/PlaceholderPage';
 import { PresenzeView } from '@/components/PresenzeView';
 
 export default function Presenze() {
