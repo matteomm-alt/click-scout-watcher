0223fd00397dabad838297c7056f28f3d90a5803
matteomm-alt <matteo.m.m@gmail.com>
2026-04-18 19:30:54 +0200

    Update Convocazioni.tsx

--- src/pages/gestionale/Convocazioni.tsx
+++ src/pages/gestionale/Convocazioni.tsx
@@ -1,13 +1,5 @@
-import { ListChecks } from 'lucide-react';
-import PlaceholderPage from '@/components/PlaceholderPage';
+import { ConvocazioniView } from '@/components/ConvocazioniView';
 
 export default function Convocazioni() {
-  return (
-    <PlaceholderPage
-      block="gestionale"
-      icon={ListChecks}
-      title="Convocazioni"
-      description="Crea distinte gara: titolari, riserve, libero. Esporta PDF/foglio gara."
-    />
-  );
+  return <ConvocazioniView />;
 }
